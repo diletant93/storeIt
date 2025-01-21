e8f140ffb804dec2e1928fc844a6e79abc49644b
Artur <artur.talimonchyk@gmail.com>
2025-01-21 18:33:39 +0100

    last fixes

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,14 +1,20 @@
 import Loader from "@/components/elements/Loader";
 import FileList from "@/components/shared/FileList";
+import FileUploader from "@/components/shared/FileUploader";
 import Sort from "@/components/shared/Sort";
-import { getTotalSize } from "@/lib/actions/file.actions";
+import { getFiles, getTotalSize } from "@/lib/actions/file.actions";
+import { getCurrentUser } from "@/lib/actions/user.actions";
 import { getFileTypesParams } from "@/lib/utils";
 import { SearchParamProps } from "@/types/props";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 export default async function Page({ params, searchParams }: SearchParamProps) {
     let type = (await params)?.type as string
     const types = getFileTypesParams(type)
-    const totalSize = await getTotalSize({types})
+    const totalSize = await getTotalSize({ types })
+    const files = await getFiles({types})
+    const currentUser = await getCurrentUser()
+    if (!currentUser) redirect('/sign-in')
     return (
         <div className="page-container">
             <section className="w-full">
@@ -18,21 +24,27 @@ export default async function Page({ params, searchParams }: SearchParamProps) {
                 <div className="total-size-section">
                     <p className="body-1 min-w-[5rem]">
                         Total: <span className="h5">
-                            {totalSize} 
+                            {totalSize}
                         </span>
                     </p>
-                    
+
                     <div className="sort-container">
                         <p className="body-1 hidden sm:block text-light-200 min-w-[5rem]">
                             Sort by:
                         </p>
-                        <Sort searchParams={searchParams}/>
+                        <Sort searchParams={searchParams} />
                     </div>
                 </div>
             </section>
-            <Suspense fallback={<Loader/>}>
-                <FileList type={type} searchParams={searchParams}/>
-            </Suspense> 
+            {files ? (
+                <Suspense fallback={<Loader />}>
+                    <FileList type={type} searchParams={searchParams} />
+                </Suspense>
+            ) : (
+                <div className=" h-full flex-center">
+                    <FileUploader accountId={currentUser.accountId} ownerId={currentUser.$id} className="min-w-[500px] min-h-[500px]  " />
+                </div>
+            )}
         </div>
     );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,37 +1,41 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Models } from "node-appwrite";
-
-import ActionDropdown from "@/components/shared/ActionDropDown";
-import { Chart } from "@/components/shared/Chart";
-import FormattedDateTime from "@/components/elements/FormattedDateTime";
-import Thumbnail from "@/components/elements/Thumbnail";
-import { Separator } from "@/components_shadcn/ui/separator";
-import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
-import { convertFileSize, getUsageSummary } from "@/lib/utils";
-import { IFileType } from "@/types/types";
 import RecentFiles from "@/components/elements/RecentFiles";
 import { Suspense } from "react";
 import Loader from "@/components/elements/Loader";
 import Stats from "@/components/shared/Stats";
+import { getFileIcon } from "@/lib/utils";
+import { getFiles } from "@/lib/actions/file.actions";
+import FileUploader from "@/components/shared/FileUploader";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
+  const files = await getFiles({})
+  const currentUser = await getCurrentUser()
+  if (!currentUser) redirect('/sign-in')
   return (
-    <div className="dashboard-container">
-      <section className="relative">
-        <Suspense fallback={<Loader/>}>
-          <Stats/>
-        </Suspense>
-      </section>
+    <>
+      {
+        files ? (
+          <div className="dashboard-container" >
+            <section className="relative">
+              <Suspense fallback={<Loader />}>
+                <Stats />
+              </Suspense>
+            </section>
+            <section className="dashboard-recent-files relative">
+              <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+              <Suspense fallback={<Loader />}>
+                <RecentFiles />
+              </Suspense>
+            </section>
+          </div >
+        ) : (
+          <div className=" h-full flex-center">
+            <FileUploader accountId={currentUser.accountId} ownerId={currentUser.$id} className="min-w-[500px] min-h-[500px]  " />
+          </div>
+        )}
 
-      {/* Recent files uploaded */}
-      <section className="dashboard-recent-files relative">
-        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
-        <Suspense fallback={<Loader/>}>
-          <RecentFiles />
-        </Suspense>
-      </section>
-    </div>
+    </>
   );
 };
 
